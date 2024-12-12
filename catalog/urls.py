6e3b6b27f7b0b7e54bf721e7689dea25631b9b98
leerkz <l3ra131007@gmail.com>
2024-12-12 18:27:38 +0300

    add project

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_info, product_list
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('product_info/<int:pk>/', product_info, name='product_info'),
+    path('home', product_list, name='product_list')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product, Category
 
 
 def home(request):
@@ -7,3 +8,26 @@ def home(request):
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def product_info(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'product_name': product.name,
+        'product_description': product.description,
+        'product_image': product.image,
+        'product_category': product.category,
+        'product_cost': product.cost,
+        'product_created_at': product.created_at,
+        'product_updated_at': product.updated_at
+
+    }
+    return render(request, template_name='product_info.html', context=context)
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request,'home.html', context=context)
